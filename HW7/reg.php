1f4102660729a083d74df9b6d2df426cceab3394
hydai <z54981220@gmail.com>
2014-04-19 09:03:48 -0400

    Reg checking for fields

--- HW7/reg.php
+++ HW7/reg.php
@@ -16,32 +16,41 @@ $nickname = $_POST['nickname'];
 $sex = $_POST['sex'];
 $email = $_POST['email'];
 
+$accRep = preg_replace("/[A-Za-z0-9]/","",$account);
 if($account == '') {
-    $errmsg_arr[] = 'You must enter your account';
+    $errmsg_arr[] = 'Account cannot be empty.';
+    $errflag = true;
+} else if ($accRep != '') {
+    $errmsg_arr[] = 'Account is invalid. Please use A-Z, 0-9, a-z.';
+    $errflag = true;
+} else if (strlen($account) < 6) {
+    $errmsg_arr[] = 'Account is too short.';
     $errflag = true;
 }
 if($password == '') {
-    $errmsg_arr[] = 'You must enter your password';
+    $errmsg_arr[] = 'Password cannot be empty.';
+    $errflag = true;
+} else if (strlen($password) < 7) {
+    $errmsg_arr[] = 'Password is too short.';
     $errflag = true;
 }
 if($name == '') {
-    $errmsg_arr[] = 'You must enter your name';
+    $errmsg_arr[] = 'Your name cannot be empty.';
     $errflag = true;
 }
 if($sex == '') {
-    $errmsg_arr[] = 'You must enter your sex';
+    $errmsg_arr[] = 'Please enter your sex.';
     $errflag = true;
 }
 if($email == '') {
-    $errmsg_arr[] = 'You must enter your email';
+    $errmsg_arr[] = 'E-mail cannot be empty.';
     $errflag = true;
 }
 
-
 if ($errflag == false) {
     // query
-    $sql = "INSERT INTO `USER`(`ID`, `ACCOUNT`, `PWD`, `NAME`, `NICKNAME`, `SEX`, `EMAIL`) VALUES ("",$account,$password,$name,$nickname,$sex,$email)";
-    mysql_query($sql);
+    $password = md5($password);
+    $sql = "INSERT INTO USER (ID, ACCOUNT, PWD, NAME, NICKNAME, SEX, EMAIL) VALUES ('','$account','$password','$name','$nickname','$sex','$email')";
     // save data to session
     $_SESSION['ACCOUNT'] = $account;
     $_SESSION['PWD'] = $password;

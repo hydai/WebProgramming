87352c4bd87935ed1ca6380b9191bb218c8092fb
hydai <z54981220@gmail.com>
2014-05-11 21:43:59 +0800

    Reformat php files

--- HW7&8/register.php
+++ HW7&8/register.php
@@ -8,18 +8,18 @@ if($_SESSION['ACCOUNT']!=null){ // 如果登入過，則直接轉到登入後頁
     echo '<meta http-equiv=REFRESH CONTENT=0;url=index.php>';
 }
 ?>
-
 <!DOCTYPE html>
 <meta http-equiv="content-type" content="text/html; charset=UTF-8" />
+
 <head>
-<title>Fakebook - Register</title>
+    <title>Fakebook - Register</title>
     <!-- use google font API -->
     <link href='http://fonts.googleapis.com/css?family=Lemon|Codystar|PT+Mono' rel='stylesheet' type='text/css'>
     <link href='main.css' rel='stylesheet' type='text/css'>
     <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
 </head>
-   <body bgcolor="#ccccff">
 
+<body bgcolor="#ccccff">
 <?php
 session_start();
 if( isset($_SESSION['ERRMSG_ARR']) && is_array($_SESSION['ERRMSG_ARR']) && count($_SESSION['ERRMSG_ARR']) >0 ) {
@@ -31,18 +31,28 @@ if( isset($_SESSION['ERRMSG_ARR']) && is_array($_SESSION['ERRMSG_ARR']) && count
     unset($_SESSION['ERRMSG_ARR']);
 }
 ?>
-      <form action="reg.php" method="post">
-         帳號：<input type="text" name="account" placeholder="A-Z, 0-9, a-z, length >= 6" autocomplete="off"><br/>
-         密碼：<input type="password" name="password" placeholder="length >= 7" autocomplete="off"><br/>
-         密碼確認：<input type="password" name="passwordrepeat" placeholder="Repeat above" autocomplete="off"><br/>
-         姓名：<input type="text" name="name" placeholder="yourName" autocomplete="off"><br/>
-         暱稱：<input type="text" name="nickname" placeholder="yourNickname" autocomplete="off"><br/>
-<!--Not support Sex nuw
+    <form action="reg.php" method="post">
+        帳號：
+        <input type="text" name="account" placeholder="A-Z, 0-9, a-z, length >= 6" autocomplete="off">
+        <br/>密碼：
+        <input type="password" name="password" placeholder="length >= 7" autocomplete="off">
+        <br/>密碼確認：
+        <input type="password" name="passwordrepeat" placeholder="Repeat above" autocomplete="off">
+        <br/>姓名：
+        <input type="text" name="name" placeholder="yourName" autocomplete="off">
+        <br/>暱稱：
+        <input type="text" name="nickname" placeholder="yourNickname" autocomplete="off">
+        <br/>
+        <!--Not support Sex nuw
          性別：<input type="text" name="sex" placeholder="Male/Female" autocomplete="off"><br/>
 -->
-         E-mail：<input type="text" name="email" placeholder="youremail@example.com" autocomplete="off"><br/>
-         <input type="submit">
-         <input type="button" onclick="window.location='login.php'" value="Cancel">
-      </form>
+        E-mail：
+        <input type="text" name="email" placeholder="youremail@example.com" autocomplete="off">
+        <br/>
+        <input type="submit">
+        <input type="button" onclick="window.location='login.php'" value="Cancel">
+    </form>
 
-</body></html>
+</body>
+
+</html>
--- HW7&8/update.php
+++ HW7&8/update.php
@@ -13,59 +13,107 @@ if($_SESSION['ACCOUNT']==null){ // 如果還沒登入過，則直接轉到登入
 
 <!DOCTYPE html>
 <meta http-equiv="content-type" content="text/html; charset=UTF-8" />
+
 <head>
     <title>Fakebook - Update</title>
     <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
     <script src="http://m101.nthu.edu.tw/~s101062124/bootstrap/js/bootstrap.min.js"></script>
     <link href='http://m101.nthu.edu.tw/~s101062124/bootstrap/css/bootstrap.min.css' rel='stylesheet' type='text/css'>
     <!-- use google font API -->
-<link href='http://fonts.googleapis.com/css?family=Open+Sans:400,600,300,700' rel='stylesheet' type='text/css'>
+    <link href='http://fonts.googleapis.com/css?family=Open+Sans:400,600,300,700' rel='stylesheet' type='text/css'>
     <link href='main.css' rel='stylesheet' type='text/css'>
 </head>
-   <body bgcolor="#ccccff">
-<nav class="navbar navbar-default navbar-fixed-top" role="navigation">
-  <div class="container-fluid">
-    <!-- Brand and toggle get grouped for better mobile display -->
-    <div class="navbar-header">
-      <a class="navbar-brand" href="index.php">Fakebook</a>
-    </div>
 
-    <!-- Collect the nav links, forms, and other content for toggling -->
-    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-      <form class="navbar-form navbar-left" role="search" action="findFriend.php" method="post">
-        <div class="form-group">
-          <input type="text" class="form-control" placeholder="Search friends" name="friendAccount" autocomplete="off">
+<body bgcolor="#ccccff">
+    <nav class="navbar navbar-default navbar-fixed-top" role="navigation">
+        <div class="container-fluid">
+            <!-- Brand and toggle get grouped for better mobile display -->
+            <div class="navbar-header">
+                <a class="navbar-brand" href="index.php">Fakebook</a>
+            </div>
+
+            <!-- Collect the nav links, forms, and other content for toggling -->
+            <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                <form class="navbar-form navbar-left" role="search" action="findFriend.php" method="post">
+                    <div class="form-group">
+                        <input type="text" class="form-control" placeholder="Search friends" name="friendAccount" autocomplete="off">
+                    </div>
+                    <button type="submit" class="btn btn-default">
+                        <span class="glyphicon glyphicon-search"></span>Search</button>
+                </form>
+                <div class="navbar-form navbar-right">
+                    <button class="btn btn-default" onclick="window.location='update.php'">
+                        <span class="glyphicon glyphicon-cog"></span>修改資料</button>
+                    <button class="btn btn-default" onclick="window.location='logout.php'">
+                        <span class="glyphicon glyphicon-road"></span>登出</button>
+                </div>
+            </div>
+            <!-- /.navbar-collapse -->
         </div>
-        <button type="submit" class="btn btn-default"><span class="glyphicon glyphicon-search"></span> Search</button>
-      </form>
-      <div class="navbar-form navbar-right">
-<button class="btn btn-default" onclick="window.location='update.php'"><span class="glyphicon glyphicon-cog"></span> 修改資料</button>
-<button class="btn btn-default" onclick="window.location='logout.php'"><span class="glyphicon glyphicon-road"></span> 登出</button>
-      </div>
-    </div><!-- /.navbar-collapse -->
-  </div><!-- /.container-fluid -->
-</nav>
+        <!-- /.container-fluid -->
+    </nav>
 
     <div id="init">
-    <div id="updateC">
-        <form action="updateF.php" method="post">
-    <table>
-        <tr><td align="left">帳號：</td><td align="right"><?php echo $_SESSION['ACCOUNT'];?></td></tr>
-        <tr><td align="left">舊密碼：</td><td align="right"><input type="password" name="oldpassword"></td></tr>
-        <tr><td align="left">新密碼：</td><td align="right"><input type="password" name="password" placeholder="length >= 7"></td></tr>
-        <tr><td align="left">密碼確認：</td><td align="right"><input type="password" name="passwordrepeat" placeholder="Repeat above" autocomplete="off"></td></tr>
-        <tr><td align="left">姓名：</td><td align="right"><input type="text" name="name" placeholder="yourName" autocomplete="off" value="<?php echo $_SESSION['NAME'];?>"></td></tr>
-        <tr><td align="left">暱稱：</td><td align="right"><input type="text" name="nickname" placeholder="yourNickname" autocomplete="off" value="<?php echo $_SESSION['NICKNAME'];?>"></td></tr>
-        <tr><td align="left">E-mail：</td><td align="right"><input type="text" name="email" placeholder="youremail@example.com" autocomplete="off" value="<?php echo $_SESSION[EMAIL];?>"></td></tr>
-        <tr>
-        <td><input type="submit" value="Submit"></td>
-         <td><input type="button" onclick="window.location='index.php'" value="Cancel"></td>
-    </tr>
-    </table>
-      </form>
-    </div>
+        <div id="updateC">
+            <form action="updateF.php" method="post">
+                <table>
+                    <tr>
+                        <td align="left">帳號：</td>
+                        <td align="right">
+                            <?php echo $_SESSION[ 'ACCOUNT'];?>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td align="left">舊密碼：</td>
+                        <td align="right">
+                            <input type="password" name="oldpassword">
+                        </td>
+                    </tr>
+                    <tr>
+                        <td align="left">新密碼：</td>
+                        <td align="right">
+                            <input type="password" name="password" placeholder="length >= 7">
+                        </td>
+                    </tr>
+                    <tr>
+                        <td align="left">密碼確認：</td>
+                        <td align="right">
+                            <input type="password" name="passwordrepeat" placeholder="Repeat above" autocomplete="off">
+                        </td>
+                    </tr>
+                    <tr>
+                        <td align="left">姓名：</td>
+                        <td align="right">
+                            <input type="text" name="name" placeholder="yourName" autocomplete="off" value="<?php echo $_SESSION['NAME'];?>">
+                        </td>
+                    </tr>
+                    <tr>
+                        <td align="left">暱稱：</td>
+                        <td align="right">
+                            <input type="text" name="nickname" placeholder="yourNickname" autocomplete="off" value="<?php echo $_SESSION['NICKNAME'];?>">
+                        </td>
+                    </tr>
+                    <tr>
+                        <td align="left">E-mail：</td>
+                        <td align="right">
+                            <input type="text" name="email" placeholder="youremail@example.com" autocomplete="off" value="<?php echo $_SESSION[EMAIL];?>">
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>
+                            <input type="submit" value="Submit">
+                        </td>
+                        <td>
+                            <input type="button" onclick="window.location='index.php'" value="Cancel">
+                        </td>
+                    </tr>
+                </table>
+            </form>
+        </div>
         <div id="errmsg">
-    <script type="text/javascript">$('#errmsg').hide();</script>
+            <script type="text/javascript">
+            $('#errmsg').hide();
+            </script>
 <?php
 session_start();
 if( isset($_SESSION['ERRMSG_ARR']) && is_array($_SESSION['ERRMSG_ARR']) && count($_SESSION['ERRMSG_ARR']) >0 ) {
@@ -79,7 +127,9 @@ if( isset($_SESSION['ERRMSG_ARR']) && is_array($_SESSION['ERRMSG_ARR']) && count
     unset($_SESSION['ERRMSG_ARR']);
 }
 ?>
-    </div>
+        </div>
     </div>
 
-</body></html>
+</body>
+
+</html>

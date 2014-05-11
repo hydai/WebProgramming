7c6b37d63bcc1232f5e083268d572c11c84dde10
hydai <z54981220@gmail.com>
2014-05-11 13:48:22 +0800

    Reformat css and php

--- HW7&8/login.php
+++ HW7&8/login.php
@@ -34,37 +34,43 @@ else {
 
 <!DOCTYPE html>
 <meta http-equiv="content-type" content="text/html; charset=UTF-8" />
+
 <head>
-<title>Fakebook - Login</title>
+    <title>Fakebook - Login</title>
     <!-- use google font API -->
     <link href='http://fonts.googleapis.com/css?family=Lemon|Codystar|PT+Mono' rel='stylesheet' type='text/css'>
     <link href='main.css' rel='stylesheet' type='text/css'>
     <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
 </head>
-   <body bgcolor="#ccccff">
+
+<body bgcolor="#ccccff">
 
     <div id="init">
         <div id="loginC">
-          <form action="login.php" method="post">
-            <table>
-            <tr>
-            <td align="left">帳號：</td>
-            <td align="right"><input type="text" name="account" autocomplete="off"></td>
-            </tr>
-            <tr>
-            <td align="left">密碼：</td>
-            <td align="right"><input type="password" name="password" autocomplete="off"></td>
-            </tr>
-            <tr>
-            <td>
-             <input type="submit" value="Log in">
-            </td>
-            <td>
-             <input type="button" onclick="window.location='register.php'" value="Sign Up">
-            </td>
-            </tr>
-            </table>
-          </form>
+            <form action="login.php" method="post">
+                <table>
+                    <tr>
+                        <td align="left">帳號：</td>
+                        <td align="right">
+                            <input type="text" name="account" autocomplete="off">
+                        </td>
+                    </tr>
+                    <tr>
+                        <td align="left">密碼：</td>
+                        <td align="right">
+                            <input type="password" name="password" autocomplete="off">
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>
+                            <input type="submit" value="Log in">
+                        </td>
+                        <td>
+                            <input type="button" onclick="window.location='register.php'" value="Sign Up">
+                        </td>
+                    </tr>
+                </table>
+            </form>
         </div>
         <div id="regC">
 <?php
@@ -78,20 +84,29 @@ if( isset($_SESSION['ERRMSG_ARR']) && is_array($_SESSION['ERRMSG_ARR']) && count
     unset($_SESSION['ERRMSG_ARR']);
 }
 ?>
-          <form action="reg.php" method="post">
-             帳號：<input type="text" name="account" placeholder="A-Z, 0-9, a-z, length >= 6" autocomplete="off"><br/>
-             密碼：<input type="password" name="password" placeholder="length >= 7" autocomplete="off"><br/>
-             密碼確認：<input type="password" name="passwordrepeat" placeholder="Repeat above" autocomplete="off"><br/>
-             姓名：<input type="text" name="name" placeholder="yourName" autocomplete="off"><br/>
-             暱稱：<input type="text" name="nickname" placeholder="yourNickname" autocomplete="off"><br/>
-        <!--Not support Sex nuw
+            <form action="reg.php" method="post">
+                帳號：
+                <input type="text" name="account" placeholder="A-Z, 0-9, a-z, length >= 6" autocomplete="off">
+                <br/>密碼：
+                <input type="password" name="password" placeholder="length >= 7" autocomplete="off">
+                <br/>密碼確認：
+                <input type="password" name="passwordrepeat" placeholder="Repeat above" autocomplete="off">
+                <br/>姓名：
+                <input type="text" name="name" placeholder="yourName" autocomplete="off">
+                <br/>暱稱：
+                <input type="text" name="nickname" placeholder="yourNickname" autocomplete="off">
+                <br/>
+                <!--Not support Sex nuw
              性別：<input type="text" name="sex" placeholder="Male/Female" autocomplete="off"><br/>
         -->
-             E-mail：<input type="text" name="email" placeholder="youremail@example.com" autocomplete="off"><br/>
-             <input type="submit">
-             <input type="button" onclick="window.location='login.php'" value="Cancel">
-          </form>
+                E-mail：
+                <input type="text" name="email" placeholder="youremail@example.com" autocomplete="off">
+                <br/>
+                <input type="submit">
+                <input type="button" onclick="window.location='login.php'" value="Cancel">
+            </form>
         </div>
     </div>
 </body>
+
 </html>
